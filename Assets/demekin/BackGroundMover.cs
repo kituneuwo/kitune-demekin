9b46e700e99b0710128492595b254ad247eb9e4f
DEMEKING728 <deme.090728@gmail.com>
2024-04-21 19:50:14 +0900

    キツネのものに用意してあったスクリプトに内容を移行、内容を若干修正

--- Assets/demekin/BackGroundMover.cs
+++ Assets/demekin/BackGroundMover.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class BackGroundMover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField]
+    private float speed = 0.1f;
     void Update()
     {
-        
+        this.transform.position += new Vector3(speed, 0, 0);
     }
 }
