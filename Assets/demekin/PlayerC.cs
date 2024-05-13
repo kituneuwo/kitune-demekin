9cbe2289e660fb35b60ead6423293104d0d69263
DEMEKING728 <deme.090728@gmail.com>
2024-05-13 18:02:44 +0900

    移動できるようになった

--- Assets/demekin/PlayerC.cs
+++ Assets/demekin/PlayerC.cs
@@ -7,9 +7,15 @@ public class PlayerC : MonoBehaviour
     [SerializeField]
     private float speed;
     [SerializeField]
+    private float MinX;
+    [SerializeField]
+    private float MaxX;
+    [SerializeField]
     private float MaxY;
     [SerializeField]
     private float MinY;
+    [SerializeField]
+    private GameObject EnemyObject;
     void Update()
     {
         if (Input.GetKey(KeyCode.UpArrow) && transform.position.y < MaxY)
@@ -19,6 +25,18 @@ public class PlayerC : MonoBehaviour
         if (Input.GetKey(KeyCode.DownArrow) && transform.position.y > MinY)
         {
             transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x > MinX)
+        {
+            transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x < MaxX)
+        {
+            transform.position += new Vector3(speed * Time.deltaTime,0, 0);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(EnemyObject, new Vector3(-15,0,0), Quaternion.identity);
         }
     }
 }
