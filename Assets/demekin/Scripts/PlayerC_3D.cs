178cdb4f9ae159d72b2e5e558f8ccca74d5fea7d
DEMEKING728 <deme.090728@gmail.com>
2024-07-07 14:59:25 +0900

    微調整2

--- Assets/demekin/Scripts/PlayerC_3D.cs
+++ Assets/demekin/Scripts/PlayerC_3D.cs
@@ -29,7 +29,7 @@ public class PlayerC_3D : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.I))
         {
-            var obj = Instantiate(EnemyObject, transform.forward * -15f + transform.position, Quaternion.Euler(0, transform.localEulerAngles.y + 180, 0));
+            var obj = Instantiate(EnemyObject, transform.forward * 15f + transform.position, Quaternion.Euler(transform.localEulerAngles.x, transform.localEulerAngles.y + 180, 0));
             obj.name = EnemyObject.name;
         }
     }
