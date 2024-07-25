e138ffae58169605a5be42de0d6183d4bb9ab35c
DEMEKING728 <deme.090728@gmail.com>
2024-07-25 18:17:00 +0900

    修正その2

--- Assets/demekin/Scripts/PlayerRotate.cs
+++ Assets/demekin/Scripts/PlayerRotate.cs
@@ -21,38 +21,41 @@ public class NewPlayerRotate : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
+        if(PlayerObject != null)
         {
-            RotateNum.z = -30;
-            RotateNum.y = transform.localEulerAngles.y + torque * Time.deltaTime;
+            if (Input.GetKey(KeyCode.D))
+            {
+                RotateNum.z = -30;
+                RotateNum.y = transform.localEulerAngles.y + torque * Time.deltaTime;
 
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                RotateNum.z = 30;
+                RotateNum.y = transform.localEulerAngles.y - torque * Time.deltaTime;
+            }
+            else
+            {
+                RotateNum.z = 0;
+            }
+            if (Input.GetKey(KeyCode.W) && (transform.localEulerAngles.x >= limitup || transform.localEulerAngles.x == 0 || transform.localEulerAngles.x <= limitdown))
+            {
+                RotateNum.x = transform.localEulerAngles.x - torque * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.S) && (transform.localEulerAngles.x <= limitdown || transform.localEulerAngles.x >= limitup))
+            {
+                RotateNum.x = transform.localEulerAngles.x + torque * Time.deltaTime;
+            }
+            if (transform.localEulerAngles.x <= limitup && transform.localEulerAngles.x > limitup - 10)
+            {
+                RotateNum.x = limitup + 0.01f;
+            }
+            if (transform.localEulerAngles.x >= limitdown && transform.localEulerAngles.x <= limitdown + 10)
+            {
+                RotateNum.x = limitdown - 0.01f;
+            }
+            transform.rotation = Quaternion.Euler(RotateNum);
+            transform.position = PlayerObject.transform.position;
         }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            RotateNum.z = 30;
-            RotateNum.y = transform.localEulerAngles.y - torque * Time.deltaTime;
-        }
-        else
-        {
-            RotateNum.z = 0;
-        }
-        if (Input.GetKey(KeyCode.W) && (transform.localEulerAngles.x >= limitup || transform.localEulerAngles.x == 0 || transform.localEulerAngles.x <= limitdown))
-        {
-            RotateNum.x = transform.localEulerAngles.x - torque * Time.deltaTime;
-        }
-        else if (Input.GetKey(KeyCode.S) && (transform.localEulerAngles.x <= limitdown || transform.localEulerAngles.x >= limitup))
-        {
-            RotateNum.x = transform.localEulerAngles.x + torque * Time.deltaTime;
-        }
-        if(transform.localEulerAngles.x <= limitup && transform.localEulerAngles.x > limitup - 10)
-        {
-            RotateNum.x = limitup + 0.01f;
-        }
-        if(transform.localEulerAngles.x >= limitdown && transform.localEulerAngles.x <= limitdown + 10)
-        {
-            RotateNum.x = limitdown - 0.01f;
-        }
-        transform.rotation = Quaternion.Euler(RotateNum);
-        transform.position = PlayerObject.transform.position;
     }
 }
