793ba3443593002a6f63c343684f9e6c8564d8c0
oohasi-kanade <super.kanata3000@gmail.com>
2024-07-29 18:19:01 +0900

    お　け　は　ざ　ま

--- Assets/start1.cs
+++ Assets/start1.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class start1 : MonoBehaviour
 {
+    [SerializeField]
+    private string SceneA;
 
     public void SwitchScene()
     {
 
-        SceneManager.LoadScene("Stage", LoadSceneMode.Single);
+        SceneManager.LoadScene(SceneA, LoadSceneMode.Single);
         Time.timeScale = 1;
 
     }
