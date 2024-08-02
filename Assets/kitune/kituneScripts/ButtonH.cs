ddab6c06d145d6ea3a0a6769e231c9f19126f27c
oohasi-kanade <super.kanata3000@gmail.com>
2024-08-02 21:27:23 +0900

    考えること大杉だろぉ

--- Assets/kitune/kituneScripts/ButtonH.cs
+++ Assets/kitune/kituneScripts/ButtonH.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class ButtonH : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject door; // ドアオブジェクト
+    public Vector3 doorOpenPosition; // ドアの開く位置
+    public float moveSpeed = 1f; // ドアの移動速度
+
+    private bool isOpening = false;
+    private Vector3 originalPosition;
+
     void Start()
     {
-        
+        if (door != null)
+        {
+            originalPosition = door.transform.position; // ドアの元の位置を保存
+        }
     }
 
-    // Update is called once per frame
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Shoot") // Shootが当たったかどうか確認
+        {
+            isOpening = true; // ドアを開くフラグを設定
+        }
+    }
+
     void Update()
     {
-        
+        if (isOpening && door != null)
+        {
+            // ドアを開く位置まで移動
+            door.transform.position = Vector3.MoveTowards(door.transform.position, doorOpenPosition, moveSpeed * Time.deltaTime);
+        }
     }
 }
