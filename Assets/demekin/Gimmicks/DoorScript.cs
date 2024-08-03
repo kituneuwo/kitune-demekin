a2e0af93223024ea2a5212ad0b12648a4eadde20
DEMEKING728 <deme.090728@gmail.com>
2024-08-03 21:23:13 +0900

    ドアの開閉

--- Assets/demekin/Gimmicks/DoorScript.cs
+++ Assets/demekin/Gimmicks/DoorScript.cs
@@ -6,15 +6,26 @@ public class DoorScript : MonoBehaviour
 {
     [SerializeField]
     private Vector3 doorOpenPosition;
-    private Vector3 doorClosePosition;
     [SerializeField]
     private float doorOpenSpeed;
+    private bool IsOpen;
     void Start()
     {
-        doorClosePosition = transform.position;
+        IsOpen = false;
+    }
+    private void Update()
+    {
+        if (IsOpen)
+        {
+            transform.localPosition += Vector3.up * doorOpenSpeed;
+            if(doorOpenPosition.y <= transform.localPosition.y)
+            {
+                IsOpen = false;
+            }
+        }
     }
     public void Open()
     {
-        transform.position = Vector3.MoveTowards(doorClosePosition, doorOpenPosition, doorOpenSpeed * Time.deltaTime);
+        IsOpen = true;
     }
 }
